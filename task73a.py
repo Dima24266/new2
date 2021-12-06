0d8fd3617f6803b9095ab021891c53bb8f822629
Dima24266 <dima24266@gmail.com>
2021-12-06 05:08:26 -0600

    1

--- task73a.py
+++
@@ -1,9 +0,0 @@
-result = {}
-
-with open('CAM_table.txt') as f:
-    for line in f:
-        line = line.split(' ')
-        if line and line[1][0].isdigit():
-            vlan, address, *other = line
-            result[vlan] = address
-print(result)
